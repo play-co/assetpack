80dbee22e40b8f40d3235f09bfee36634b4291c1
Mat Groves <mat.groves@play.co>
2024-04-25 12:31:38 +0100

    docs

--- packages/core/src/utils/generateCacheName.ts
+++ packages/core/src/utils/generateCacheName.ts
@@ -1,10 +1,21 @@
 import type { AssetPackConfig } from '../config';
 import objectHash from 'object-hash';
 
+/**
+ * Returns a unique name based on the hash generated from the config
+ * this takes into account the following:
+ * - pipes and their options,
+ * - entry and output paths.
+ * - assetSettings options
+ * - ignore options
+ *
+ * @param options - The asset pack config
+ * @returns - A unique name based on the hash generated from the config
+ */
 export function generateCacheName(options: AssetPackConfig)
 {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { pipes, cache, ...configWithoutPlugins } = options;
+    const { pipes, cache, logLevel, ...configWithoutPlugins } = options;
 
     const optionsToHash: any = {
         ...configWithoutPlugins,
